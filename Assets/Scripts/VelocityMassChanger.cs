be282c09df3c6bc4554e9bde6372bdf103062f80
nwill7 <nwill711@gmail.com>
2022-05-18 13:39:50 -0700

    Edit Planet panel working.

--- Assets/Scripts/VelocityMassChanger.cs
+++ Assets/Scripts/VelocityMassChanger.cs
@@ -6,24 +6,68 @@ using UnityEngine.UI;
 
 public class VelocityMassChanger : MonoBehaviour
 {
-    public Slider x_vel, y_vel, z_vel;
-    public TMP_InputField massInput;
+    public TMP_InputField x_vel, y_vel, z_vel, massInput;
+    public Toggle orbitToggle;
 
-    void onXVelocityChange(float value)
+    private void Start()
     {
-
-    }
-    void onYVelocityChange(float value)
-    {
-
-    }
-    void onZVelocityChange(float value)
-    {
-
+        PlanetariumControl.Instance.velocityMassChanger = this;
     }
 
-    void onMassInputChange(string value)
+    public void onXVelocityChange(string value)
     {
+        float newVel = float.Parse(value);
+        if (PlanetariumControl.Instance.selectedPlanet is not null)
+        {
+            Body body = PlanetariumControl.Instance.selectedPlanet.GetComponent<Body>();
+            body.UpdateVelocityX(newVel);
+        }
+    }
+    public void onYVelocityChange(string value)
+    {
+        float newVel = float.Parse(value);
+        if (PlanetariumControl.Instance.selectedPlanet is not null)
+        {
+            Body body = PlanetariumControl.Instance.selectedPlanet.GetComponent<Body>();
+            body.UpdateVelocityY(newVel);
+        }
+    }
+    public void onZVelocityChange(string value)
+    {
+        float newVel = float.Parse(value);
+        if (PlanetariumControl.Instance.selectedPlanet is not null)
+        {
+            Body body = PlanetariumControl.Instance.selectedPlanet.GetComponent<Body>();
+            body.UpdateVelocityZ(newVel);
+        }
+    }
 
+    public void onMassInputChange(string value)
+    {
+        float newMass = float.Parse(value);
+        if (PlanetariumControl.Instance.selectedPlanet is not null)
+        {
+            Body body = PlanetariumControl.Instance.selectedPlanet.GetComponent<Body>();
+            body.Mass = newMass;
+        }
+    }
+
+    public void onShowOrbitChange(bool value)
+    {
+        if (PlanetariumControl.Instance.selectedPlanet is not null)
+        {
+            Body body = PlanetariumControl.Instance.selectedPlanet.GetComponent<Body>();
+            body.EnabledOrbitVisual = value;
+        }
+    }
+
+    public void updateDisplayedValues()
+    {
+        Body body = PlanetariumControl.Instance.selectedPlanet.GetComponent<Body>();
+        x_vel.text = body.getXVelocity().ToString();
+        y_vel.text = body.getYVelocity().ToString();
+        z_vel.text = body.getZVelocity().ToString();
+        massInput.text = body.Mass.ToString();
+        orbitToggle.isOn = body.EnabledOrbitVisual;
     }
 }
