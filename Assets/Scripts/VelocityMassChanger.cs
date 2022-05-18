2dae2813bf76cf39f343e84dfafdb611833dd45e
Nick Barrios <nick@nicholasbarrios.com>
2022-05-18 14:12:25 -0700

    Change the edit planet panel design. Add on change methods to VelocityMassChanger

--- Assets/Scripts/VelocityMassChanger.cs
+++ Assets/Scripts/VelocityMassChanger.cs
@@ -2,18 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class VelocityMassChanger : MonoBehaviour
 {
-    public TMP_InputField X, Y, Z;
-    public GameObject planet;
+    public Slider x_vel, y_vel, z_vel;
+    public TMP_InputField massInput;
 
-    public void Accept_Click(){
-        Vector3 temp = new Vector3();
-        temp.x = string.IsNullOrEmpty(X.text) ? 0 : float.Parse(X.text);
-        temp.y = string.IsNullOrEmpty(Y.text) ? 0 : float.Parse(Y.text);
-        temp.z = string.IsNullOrEmpty(Z.text) ? 0 : float.Parse(Z.text);
+    void onXVelocityChange(float value)
+    {
 
-        //planet.Body.Velocity = temp;
+    }
+    void onYVelocityChange(float value)
+    {
+
+    }
+    void onZVelocityChange(float value)
+    {
+
+    }
+
+    void onMassInputChange(string value)
+    {
+
     }
 }
